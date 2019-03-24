27ad76ad1e8e3107125a30bf5d8ee4d554cdaae5
Alex Hammel <ahammel87@gmail.com>
2019-03-24 13:33:50 -0700

    ✨ At least MAX_TIME_SECONDS between events (#2)
    * ✨ At least MAX_TIME_SECONDS between events
    
    * The problem: events are occurring one after the other.
    
    * Proposed solution: add a buffer of at least MAX_TIME_SECONDS between
    events.
    
    * 🐛✨ Make MIN_TIME_BETWEEN_EVENTS configurable

--- waywo-event-scheduler.py
+++ waywo-event-scheduler.py
@@ -7,19 +7,82 @@ import random
 import boto3
 
 MAX_TIME_SECONDS = int(os.environ["MAX_TIME_SECONDS"])
+MIN_SECONDS_BETWEEN_EVENTS = int(
+    os.environ.get("MIN_SECONDS_BETWEEN_EVENTS", 0))
 EVENT_NAMESPACE = os.environ["EVENT_NAMESPACE"]
 CLIENT = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-    event_time = datetime(
+    now = datetime(
         *time.strptime(event["time"], "%Y-%m-%dT%H:%M:%SZ")[:6]
     ).timestamp()
 
-    print("The time is now {}".format(event_time))
-    print("The event will be scheduled no sooner than {}".format(
-        MAX_TIME_SECONDS))
+    upper_bound = now + MAX_TIME_SECONDS
+    lower_bound = now - MIN_SECONDS_BETWEEN_EVENTS
 
-    schedule_event_at = event_time + (random.randrange(MAX_TIME_SECONDS))
+    print("The time is now {}".format(now))
+
+    print("Checking for events already scheduled within the time bound...")
+
+    response = CLIENT.query(
+        TableName = 'scheduled-events',
+        KeyConditionExpression = \
+            "EventNamespace = :namespace AND EventTime BETWEEN :lower_bound AND :upper_bound",
+        FilterExpression = "EventExecutedAt = :null",
+        ExpressionAttributeValues = {
+            ":namespace": {
+                "S": EVENT_NAMESPACE
+            },
+            ":lower_bound": {
+                "N": str(lower_bound)
+            },
+            ":upper_bound": {
+                    "N": str(upper_bound)
+            },
+            ":null": {
+                "NULL": True
+            }
+        }
+    )
+
+    print("DyanmobDB response: {}".format(response))
+
+    most_recent_event_time = max(
+        int(event["EventTime"]["N"])
+        for event in response["Items"]) if response["Items"] else None
+
+    if (most_recent_event_time and (most_recent_event_time >= now)):
+        print(
+            "Event already scheduled at {}. "
+            "This is within {} seconds. Exiting".format(
+                most_recent_event_time,
+                MAX_TIME_SECONDS))
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                "namespace": EVENT_NAMESPACE,
+                "event_scheduled": False,
+                "event_max_offset": MAX_TIME_SECONDS,
+                "min_seconds_between_events": MIN_SECONDS_BETWEEN_EVENTS,
+            })
+        }
+
+    lower_bound_event_time = max(
+        now,
+        most_recent_event_time + MIN_SECONDS_BETWEEN_EVENTS) \
+        if most_recent_event_time else now
+
+    upper_bound_event_time = now + MAX_TIME_SECONDS
+
+    print("Choosing an event time between {} and {}".format(
+        lower_bound_event_time, upper_bound_event_time))
+
+    schedule_event_at = random.randrange(
+        lower_bound_event_time,
+        upper_bound_event_time)
+
+    print("Scheduling event at {}".format(schedule_event_at))
 
     scheduled_event = CLIENT.put_item(
         TableName = "scheduled-events",
@@ -31,7 +94,7 @@ def lambda_handler(event, context):
                 "N": str(schedule_event_at),
             },
             "EventScheduledAt": {
-                "N": str(event_time),
+                "N": str(now),
             },
             "EventMaxOffset": {
               "N": str(MAX_TIME_SECONDS),
@@ -42,14 +105,18 @@ def lambda_handler(event, context):
         }
     )
 
-    print("Event scheduled at {}".format(schedule_event_at))
+    print("DynamoDB response: {}".format(scheduled_event))
 
     return {
         'statusCode': 200,
         'body': json.dumps({
             "namespace": EVENT_NAMESPACE,
             "event_time": schedule_event_at,
-            "event_scheduled_at": event_time,
-            "event_max_offset": MAX_TIME_SECONDS
+            "event_scheduled": True,
+            "event_scheduled_at": now,
+            "event_max_offset": MAX_TIME_SECONDS,
+            "min_seconds_between_events": MIN_SECONDS_BETWEEN_EVENTS,
+            "event_time_lower_bound": lower_bound_event_time,
+            "event_time_upper_bound": upper_bound_event_time,
         })
     }
